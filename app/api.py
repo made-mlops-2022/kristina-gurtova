e08b1b3d934981ad84beed85417979223511eae2
questina <chrismatis123@gmail.com>
2022-11-20 21:50:40 +0300

    predict and small validation

--- app/api.py
+++ app/api.py
@@ -1,8 +1,12 @@
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI, Response
 import pickle
 import asyncio
 from s3 import S3Client
 from fastapi import status, HTTPException
+
+from app.schemas import InputFeatures, Input
 
 app = FastAPI()
 model = None
@@ -28,7 +32,7 @@ async def download_model():
 
 
 @app.get("/health")
-def check_health(response: Response, status_code=status.HTTP_200_OK):
+def check_health(status_code=status.HTTP_200_OK):
     if model is None:
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
@@ -36,3 +40,19 @@ def check_health(response: Response, status_code=status.HTTP_200_OK):
         )
     else:
         return {'message': 'Service is ready to accept requests!'}
+
+
+@app.post("/predict")
+def predict(request: Input):
+    X = pd.DataFrame(columns=["age", "sex", "cp", "trestbps",
+                              "chol", "fbs", "restecg", "thalach",
+                              "exang", "oldpeak", "slope",
+                              "ca", "thal"])
+    for row in request.data:
+        X.loc[len(X.index)] = [row.age, row.sex, row.cp,
+                      row.trestbps, row.chol, row.fbs,
+                      row.restecg, row.thalach, row.exang,
+                      row.oldpeak, row.slope, row.ca,
+                      row.thal]
+    y_pred = model.predict(X)
+    return {"target": y_pred.tolist()}
---
+++ app/schemas.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class InputFeatures(BaseModel):
+    age: int = Field(ge=0, le=100)
+    sex: int = Field(ge=0, le=1)
+    cp: int = Field(ge=0, le=3)
+    trestbps: int
+    chol: int
+    fbs: int = Field(ge=0, le=1)
+    restecg: int = Field(ge=0, le=1)
+    thalach: int
+    exang: int = Field(ge=0, le=1)
+    oldpeak: float
+    slope: int = Field(ge=0, le=2)
+    ca: int = Field(ge=0, le=2)
+    thal: int = Field(ge=0, le=2)
+
+
+class Input(BaseModel):
+    data: List[InputFeatures]
